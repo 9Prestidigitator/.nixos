b70da6ebd575a356b2c66a2c1b6641066fb0db79
9Prestidigitator <9Prestidigitator@gmail.com>
2025-12-01 23:30:12 -0500

    added nvidia support

--- hosts/KingSpec/default.nix
+++ hosts/KingSpec/default.nix
@@ -33,6 +33,14 @@
     networkmanager.enable = true;
   };
 
+  services.xserver.videoDrivers = ["nvidia"];
+
+  hardware.nvidia = {
+    modesetting.enable = true;
+    open = true;
+    nvidiaSettings = true;
+  };
+
   hardware.bluetooth.enable = true;
   services.blueman.enable = true;
 
