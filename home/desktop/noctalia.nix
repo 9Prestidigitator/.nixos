8804ff927e1d2c16a61b7280e11b039479e43ebf
9Prestidigitator <9Prestidigitator@gmail.com>
2025-12-03 00:54:01 -0500

    noctalia calendar fi

--- home/desktop/noctalia.nix
+++ home/desktop/noctalia.nix
@@ -10,26 +10,6 @@
     inputs.noctalia.homeModules.default
   ];
 
-  services.power-profiles-daemon.enable = true;
-  services.gnome.evolution-data-server.enable = true;
-
-  environment.systemPackages = with pkgs; [
-    (python3.withPackages (pyPkgs: with pyPkgs; [pygobject3]))
-  ];
-
-  environment.sessionVariables = {
-    GI_TYPELIB_PATH = lib.makeSearchPath "lib/girepository-1.0" (
-      with pkgs; [
-        evolution-data-server
-        libical
-        glib.out
-        libsoup_3
-        json-glib
-        gobject-introspection
-      ]
-    );
-  };
-
   programs.noctalia-shell = with config.lib.stylix.colors; {
     enable = true;
     colors = {
--- modules/desktop.nix
+++ modules/desktop.nix
@@ -30,8 +30,6 @@
 
     programs.niri.enable = true;
 
-    services.noctalia-shell.enable = true;
-
     programs.chromium = {
       enable = true;
       extensions = [
@@ -68,7 +66,24 @@
       openFirewall = true;
     };
 
+    services.noctalia-shell.enable = true;
+
+    services.gnome.evolution-data-server.enable = true;
+    environment.sessionVariables = {
+      GI_TYPELIB_PATH = lib.makeSearchPath "lib/girepository-1.0" (
+        with pkgs; [
+          evolution-data-server
+          libical
+          glib.out
+          libsoup_3
+          json-glib
+          gobject-introspection
+        ]
+      );
+    };
+
     environment.systemPackages = with pkgs; [
+      (python3.withPackages (pyPkgs: with pyPkgs; [pygobject3]))
       moonlight-qt
       brave
       kitty
