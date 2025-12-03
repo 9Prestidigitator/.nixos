d13e68f4da6acc75fbc41ad81249c89d7215e001
9Prestidigitator <9Prestidigitator@gmail.com>
2025-12-03 12:26:48 -0500

    trying to fix dsp issue

--- users/max/nixos.nix
+++ users/max/nixos.nix
@@ -1,6 +1,6 @@
 {
   users.users.max = {
     isNormalUser = true;
-    extraGroups = ["wheel" "audio" "realtime" "uinput" "libvirtd"];
+    extraGroups = ["wheel" "audio" "rtkit" "realtime" "uinput" "libvirtd"];
   };
 }
