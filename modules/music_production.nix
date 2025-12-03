2bf151dd4c93bf9ef1573e7688d80ee290a357dd
9Prestidigitator <9Prestidigitator@gmail.com>
2025-12-03 14:59:44 -0500

    added musnix for better audio performance

--- modules/music_production.nix
+++ modules/music_production.nix
@@ -5,11 +5,19 @@
   config,
   ...
 }: {
+  imports = [
+    inputs.musnix.nixosModules.musnix
+  ];
+
   options = {
     musicprod.enable = lib.mkEnableOption "Enables music production configuration.";
   };
 
   config = lib.mkIf config.musicprod.enable {
+    musnix = {
+      enable = true;
+      # kernel.realtime = true;
+    };
     environment.systemPackages = with pkgs; [
       bitwig-studio
       wineWowPackages.yabridge
