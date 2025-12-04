c1d32ae6001b60b1136ebb758681cbdaf6bb7419
9Prestidigitator <9Prestidigitator@gmail.com>
2025-12-04 00:22:31 -0500

    better stylix stability

--- flake.nix
+++ flake.nix
@@ -19,7 +19,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     stylix = {
-      url = "github:nix-community/stylix";
+      url = "github:nix-community/stylix/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
