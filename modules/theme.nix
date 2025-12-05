777bcf5dad572132140e0d960c11d5ca31dc64fc
9Prestidigitator <9Prestidigitator@gmail.com>
2025-12-05 15:20:13 -0500

    theme

--- modules/theme.nix
+++ modules/theme.nix
@@ -12,7 +12,7 @@
   stylix = {
     enable = true;
     autoEnable = true;
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/ayu-dark.yaml";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/ashes.yaml";
     polarity = "dark";
     cursor = {
       size = 24;
