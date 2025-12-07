526dee41821fb2e60359c0d2cf8db9737bfb6505
9Prestidigitator <9Prestidigitator@gmail.com>
2025-12-06 23:08:12 -0500

    removing ssh pubkeys for now

--- users/max/nixos.nix
+++ users/max/nixos.nix
@@ -3,8 +3,8 @@
     isNormalUser = true;
     extraGroups = ["wheel" "audio" "rtkit" "realtime" "uinput" "libvirtd" "cdrom"];
     openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINyAW3sKgu62YpcHJrV1wKaBB1Ck+Y2qAbGyW2TZA+Yq"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOyVUMmHaCovbd5BmyTkQ0MVReUng8Aktatjf0XAJDRp"
+      # "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINyAW3sKgu62YpcHJrV1wKaBB1Ck+Y2qAbGyW2TZA+Yq"
+      # "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOyVUMmHaCovbd5BmyTkQ0MVReUng8Aktatjf0XAJDRp"
     ];
   };
 }
