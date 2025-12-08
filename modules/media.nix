ab3474cf0ac4ae0ba569100a6ef2bcc68593e2dc
9Prestidigitator <9Prestidigitator@gmail.com>
2025-12-08 13:33:34 -0500

    better formatting

--- modules/media.nix
+++ modules/media.nix
@@ -4,14 +4,7 @@
   inputs,
   config,
   ...
-}: let
-  libbluray = pkgs.libbluray.override {
-    withAACS = true;
-    withBDplus = true;
-    withJava = true;
-  };
-  myVlc = pkgs.vlc.override {inherit libbluray;};
-in {
+}: {
   options = {
     media.enable = lib.mkEnableOption "Enable extra media packages";
   };
@@ -19,17 +12,25 @@ in {
   config = lib.mkIf config.media.enable {
     services.printing.drivers = [pkgs.hplipWithPlugin];
 
-    environment.systemPackages = with pkgs; [
-      qbittorrent
-      mpv
-      obs-studio
-      myVlc
-      javaPackages.compiler.openjdk17
-      jre
-      makemkv
-      handbrake
-      ffmpeg
-      zathura
-    ];
+    environment.systemPackages = let
+      libbluray = pkgs.libbluray.override {
+        withAACS = true;
+        withBDplus = true;
+        withJava = true;
+      };
+      myVlc = pkgs.vlc.override {inherit libbluray;};
+    in
+      with pkgs; [
+        qbittorrent
+        obs-studio
+        zathura
+        mpv
+        myVlc
+        javaPackages.compiler.openjdk17
+        jre
+        makemkv
+        handbrake
+        ffmpeg
+      ];
   };
 }
