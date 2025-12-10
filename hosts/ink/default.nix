3e1798e6907dca838bf46d9325934c044b71523e
9Prestidigitator <9Prestidigitator@gmail.com>
2025-12-10 01:36:56 -0500

    added window drive

--- hosts/ink/default.nix
+++ hosts/ink/default.nix
@@ -36,14 +36,8 @@
     };
   };
 
-  # fileSystems."/mnt/win" = {
-  #   device = "/dev/disk/by-uuid/01DA1996D2DABBB0";
-  #   fsType = "ntfs-3g";
-  #   options = ["nofail" "uid=1000" "gid=100" "umask=022"];
-  # };
-
-  fileSystems."/mnt/1tb_hdd" = {
-    device = "/dev/disk/by-uuid/7E90B7D790B7945D";
+  fileSystems."/mnt/win" = {
+    device = "/dev/disk/by-uuid/01DC699B1D247C50";
     fsType = "ntfs-3g";
     options = ["nofail" "uid=1000" "gid=100" "umask=022"];
   };
@@ -52,6 +46,12 @@
     device = "/dev/disk/by-uuid/da22c9ae-04ab-4c7f-a248-e90940025e29";
     fsType = "ext4";
     options = ["defaults"];
+  };
+
+  fileSystems."/mnt/1tb_hdd" = {
+    device = "/dev/disk/by-uuid/7E90B7D790B7945D";
+    fsType = "ntfs-3g";
+    options = ["nofail" "uid=1000" "gid=100" "umask=022"];
   };
 
   services.udev.extraRules = ''
