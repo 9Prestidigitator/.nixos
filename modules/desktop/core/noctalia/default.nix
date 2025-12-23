6ed9870ec6ad605cd09ef679effbbed8464e9ec3
9Prestidigitator <9prestidigitator@gmail.com>
2025-12-23 14:47:09 -0500

    test

--- modules/desktop/core/noctalia/default.nix
+++ modules/desktop/core/noctalia/default.nix
@@ -9,15 +9,12 @@
   isNiri = cfg.enable && cfg.mode == "niri";
   isHyprland = cfg.enable && cfg.mode == "hyprland";
 in {
-  imports = [
-    inputs.noctalia.nixosModules.default
-  ];
 
   config = lib.mkIf (isNiri || isHyprland) {
     services.noctalia-shell.enable = true;
 
     home-manager.sharedModules = [
-      inputs.noctalia.homeModules.default
+      # inputs.noctalia.homeModules.default
       ./settings.nix
       ./bar.nix
       ./dock.nix
