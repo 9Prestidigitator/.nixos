32f3816a3ef5623135d824bef1f291e9ca04e37c
9Prestidigitator <9prestidigitator@gmail.com>
2025-12-22 22:31:56 -0500

    test

--- hosts/surface/default.nix
+++ hosts/surface/default.nix
@@ -82,7 +82,7 @@ in {
       };
     };
     kernelParams = ["mem_sleep_default=deep" "kernel.nmi_watchdog=0" "vm.dirty_writeback_centisecs=1500"];
-    kernelPackages = pkgs.linuxPackages_surface;
+    # kernelPackages = pkgs.linuxPackages_surface;
     extraModprobeConfig = ''
       options i915 enable_fbc=1 enable_rc6=1 modeset=1
       options snd_hda_intel power_save=1
