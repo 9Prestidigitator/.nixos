fa1d389d061140e7e8e851efe48d0b20f61fe704
9Prestidigitator <9prestidigitator@gmail.com>
2025-12-23 11:21:39 -0500

    really whoops

--- users/max/desktop/default.nix
+++ users/max/desktop/default.nix
@@ -5,25 +5,31 @@
   pkgs,
   osConfig,
   ...
-}: {
-  programs.noctalia-shell = {
-    settings = {
-      dock = {
-        pinnedApps =
-          [
-            "kitty"
-            "org.gnome.Nautilus"
-            "brave-browser"
-          ]
-          ++ lib.optionals osConfig.desktop.gaming.enable [
-            "steam"
-          ]
-          ++ lib.optionals osConfig.desktop.comms.enable [
-            "discord"
-          ]
-          ++ lib.optionals osConfig.desktop.musicprod.enable [
-            "com.bitwig.BitwigStudio"
-          ];
+}: let
+  cfg = osConfig.desktop;
+  isNiri = cfg.enable && cfg.mode == "niri";
+  isHyprland = cfg.enable && cfg.mode == "hyprland";
+in {
+  config = lib.mkIf (isNiri || isHyprland) {
+    programs.noctalia-shell = {
+      settings = {
+        dock = {
+          pinnedApps =
+            [
+              "kitty"
+              "org.gnome.Nautilus"
+              "brave-browser"
+            ]
+            ++ lib.optionals osConfig.desktop.gaming.enable [
+              "steam"
+            ]
+            ++ lib.optionals osConfig.desktop.comms.enable [
+              "discord"
+            ]
+            ++ lib.optionals osConfig.desktop.musicprod.enable [
+              "com.bitwig.BitwigStudio"
+            ];
+        };
       };
     };
   };
