b754f8137819c7c5a775309f23381460719717d7
9Prestidigitator <9prestidigitator@gmail.com>
2025-12-26 14:41:07 -0500

    trying new stuff

--- modules/desktop/core/hyprland/default.nix
+++ modules/desktop/core/hyprland/default.nix
@@ -53,7 +53,7 @@ in {
         programs.caelestia = {
           enable = true;
           systemd = {
-            enable = false;
+            enable = true;
             target = "graphical-session.target";
             environment = [];
           };
@@ -86,11 +86,28 @@ in {
               "$mainMod, K, movefocus, u"
               "$mainMod, J, movefocus, d"
 
+              "$mainMod, Return, exec, $terminal"
+
               "$mainMod, escape, exec, caelestia shell lock lock"
 
-              "$mainMod CONTROL, space, togglefloating"
+              "$mainMod, TAB, workspace, previous"
+              "$mainMod, D, workspace, +1"
+              "$mainMod, U, workspace, -1"
 
-              "$mainMod, Return, exec, $terminal"
+              "$mainMod CONTROL, space, togglefloating"
+              "$mainMod, F, fullscreen"
+              "$mainMod, Q, killactive"
+
+              "$mainMod, 1, workspace, 1"
+              "$mainMod, 2, workspace, 2"
+              "$mainMod, 3, workspace, 3"
+              "$mainMod, 4, workspace, 4"
+              "$mainMod, 5, workspace, 5"
+              "$mainMod, 6, workspace, 6"
+              "$mainMod, 7, workspace, 7"
+              "$mainMod, 8, workspace, 8"
+              "$mainMod, 9, workspace, 9"
+              "$mainMod, 0, workspace, 10"
             ];
           };
         };
