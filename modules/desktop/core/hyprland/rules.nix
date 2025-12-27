53ea2e2fa4d00886f4cb38c42c46f9522e14007a
9Prestidigitator <9prestidigitator@gmail.com>
2025-12-26 20:13:46 -0500

    smart gaps

--- modules/desktop/core/hyprland/rules.nix
+++ modules/desktop/core/hyprland/rules.nix
@@ -7,15 +7,17 @@
 }: {
   wayland.windowManager.hyprland.settings = {
     workspace = [
-      "w[tv1], gapsout:0, gapsin:0"
-      "f[1], gapsout:0, gapsin:0"
+      # smart gaps: doesn't play nice with default caelestia
+      # "w[tv1], gapsout:0, gapsin:0"
+      # "f[1], gapsout:0, gapsin:0"
     ];
 
     windowrule = [
-      "bordersize 0, floating:0, onworkspace:w[tv1]"
-      "rounding 0, floating:0, onworkspace:w[tv1]"
-      "bordersize 0, floating:0, onworkspace:f[1]"
-      "rounding 0, floating:0, onworkspace:f[1]"
+      # smart gaps: doesn't play nice with default caelestia
+      # "bordersize 0, floating:0, onworkspace:w[tv1]"
+      # "rounding 0, floating:0, onworkspace:w[tv1]"
+      # "bordersize 0, floating:0, onworkspace:f[1]"
+      # "rounding 0, floating:0, onworkspace:f[1]"
 
       # Fix some dragging issues with XWayland
       "nofocus,class:^$,title:^$,xwayland:1,floating:1,fullscreen:0,pinned:0"
