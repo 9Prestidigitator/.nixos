dfaedb3785f86734696274a51bb8b917db728a79
9Prestidigitator <9prestidigitator@gmail.com>
2026-01-15 22:38:55 -0500

    chromebook

---
+++ hosts/book/default.nix
@@ -0,0 +1,80 @@
+{
+  pkgs,
+  lib,
+  inputs,
+  ...
+}: {
+  imports = [
+    ../../modules
+    ./hardware-configuration.nix
+  ];
+
+  networking.hostName = "book";
+  networking.networkmanager.enable = true;
+
+  desktop = {
+    enable = true;
+    wayCompositor = "niri";
+    x11WM = "i3";
+    comms.enable = true;
+    media.enable = true;
+  };
+
+  boot = {
+    loader = {
+      grub = {
+        enable = true;
+        device = "nodev";
+        efiSupport = true;
+        useOSProber = true;
+        theme = "${pkgs.kdePackages.breeze-grub}/grub/themes/breeze";
+      };
+      efi.canTouchEfiVariables = true;
+    };
+  };
+
+  services.udev.extraRules = ''
+    KERNEL=="sr[0-9]*", GROUP="cdrom", MODE="0660"
+  '';
+
+  services.thermald.enable = true;
+  services.blueman.enable = true;
+  services.libinput.enable = true;
+  services.acpid.enable = true;
+
+  security.polkit.enable = true;
+  security.rtkit.enable = true;
+  # Enable touchpad support (enabled default in most desktopManager).
+
+  services.logind = {
+    settings.Login = {
+      HandleLidSwitch = "suspend";
+      HandleLidSwitchDocked = "ignore";
+      HandlePowerKey = "suspend";
+    };
+  };
+
+  networking.wg-quick.interfaces.wg0 = {
+    configFile = "/secret/wg0.conf";
+    autostart = false;
+  };
+
+  # This option defines the first version of NixOS you have installed on this particular machine,
+  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
+  #
+  # Most users should NEVER change this value after the initial install, for any reason,
+  # even if you've upgraded your system to a new NixOS release.
+  #
+  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
+  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
+  # to actually do that.
+  #
+  # This value being lower than the current NixOS release does NOT mean your system is
+  # out of date, out of support, or vulnerable.
+  #
+  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
+  # and migrated your data accordingly.
+  #
+  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+  system.stateVersion = "25.11"; # Did you read the comment?
+}
