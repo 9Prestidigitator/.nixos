7f8aeb2c6bd9e2d9619f732c7f072b502dab09f6
9Prestidigitator <9prestidigitator@gmail.com>
2026-01-19 02:06:11 -0500

    updated a whole bunch

--- users/max/default.nix
+++ users/max/default.nix
@@ -4,6 +4,7 @@
     extraGroups = ["wheel" "audio" "rtkit" "realtime" "uinput" "libvirtd" "cdrom"];
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE+gNhwiI6VWUfLepEQJgtob1G3IiJNckD5R5HkRfhE2"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPSkPOBCXRRKK/4NKlwdGB0zVFZjlfwE9wNeercfVDGX"
       "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBIE7a6cld/4ZWzzQT70YA/DWgt7lVf9NcWTqOdDi58onE8qrwjoH7PkmY8Ygd5HJQT0/a2jXPhnq7cCEHFtPCPk="
     ];
   };
