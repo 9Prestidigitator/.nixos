aefb5014b25a7a8085185f923863ea9d5a6de505
9Prestidigitator <9prestidigitator@gmail.com>
2026-03-26 03:09:20 -0400

    updated derivation and module

--- modules/pkgs.nix
+++ modules/pkgs.nix
@@ -3,49 +3,51 @@
     packages.overwitch = pkgs.callPackage ../pkgs/overwitch.nix {};
   };
 
-  flake.overlays.default = final: prev: {
-    overwitch = final.callPackage ../pkgs/overwitch.nix {};
-  };
-
-  flake.nixosModules.overwitch = {
-    lib,
-    config,
-    pkgs,
-    ...
-  }: let
-    cfg = config.services.overwitch;
-  in {
-    options.services.overwitch = {
-      enable = lib.mkEnableOption "Overwitch Overbridge daemon";
-
-      package = lib.mkOption {
-        type = lib.types.package;
-        default = pkgs.overwitch;
-        description = "Overwitch package to use";
-      };
+  flake = {
+    overlays.default = final: prev: {
+      overwitch = final.callPackage ../pkgs/overwitch.nix {};
     };
 
-    config = lib.mkIf cfg.enable {
-      environment.systemPackages = [cfg.package];
-      services = {
-        udev.packages = [cfg.package];
-        udev.extraRules = ''
-          SUBSYSTEM=="usb", ATTR{idVendor}=="1935", MODE="0666"
-        '';
+    nixosModules.overwitch = {
+      lib,
+      config,
+      pkgs,
+      ...
+    }: let
+      cfg = config.services.overwitch;
+    in {
+      options.services.overwitch = {
+        enable = lib.mkEnableOption "Overwitch Overbridge daemon";
+
+        package = lib.mkOption {
+          type = lib.types.package;
+          default = pkgs.overwitch;
+          description = "Overwitch package to use";
+        };
       };
 
-      # systemd.user.services.overwitch = {
-      #   description = "Overwitch overbridge Daemon";
-      #   wantedBy = ["default.target"];
-      #   after = [
-      #     "pipewire.service"
-      #   ];
-      #
-      #   serviceConfig = {
-      #     ExecStart = "${cfg.package}/bin/overwitch-service";
-      #     Restart = "on-failure";
-      #   };
-      # };
+      config = lib.mkIf cfg.enable {
+        environment.systemPackages = [cfg.package];
+        services = {
+          udev.packages = [cfg.package];
+          udev.extraRules = ''
+            SUBSYSTEM=="usb", ATTR{idVendor}=="1935", MODE="0666"
+          '';
+        };
+
+        systemd.user.services.overwitch = {
+          description = "Overwitch overbridge Daemon";
+          wantedBy = ["default.target"];
+          after = [
+            "pipewire.service"
+          ];
+
+          serviceConfig = {
+            ExecStart = "${cfg.package}/bin/overwitch-service";
+            Restart = "on-failure";
+          };
+        };
+      };
     };
   };
 }
