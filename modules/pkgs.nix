a70feba73451a6480685d114b5492b4c5ca094c5
9Prestidigitator <9prestidigitator@gmail.com>
2026-03-26 03:01:21 -0400

    Added module

--- modules/pkgs.nix
+++ modules/pkgs.nix
@@ -7,9 +7,45 @@
     overwitch = final.callPackage ../packages/overwitch.nix {};
   };
 
-  flake.nixosModules.overwitch = {pkgs, ...}: {
-    environment.systemPackages = [pkgs.overwitch];
+  flake.nixosModules.overwitch = {
+    lib,
+    config,
+    pkgs,
+    ...
+  }: let
+    cfg = config.services.overwitch;
+  in {
+    options.services.overwitch = {
+      enable = lib.mkEnableOption "Overwitch Overbridge daemon";
 
-    services.udev.packages = [pkgs.overwitch];
+      package = lib.mkOption {
+        type = lib.types.package;
+        default = pkgs.overwitch;
+        description = "Overwitch package to use";
+      };
+    };
+
+    config = lib.mkIf cfg.enable {
+      environment.systemPackages = [cfg.package];
+      services = {
+        udev.packages = [cfg.package];
+        udev.extraRules = ''
+          SUBSYSTEM=="usb", ATTR{idVendor}=="1935", MODE="0666"
+        '';
+      };
+
+      # systemd.user.services.overwitch = {
+      #   description = "Overwitch overbridge Daemon";
+      #   wantedBy = ["default.target"];
+      #   after = [
+      #     "pipewire.service"
+      #   ];
+      #
+      #   serviceConfig = {
+      #     ExecStart = "${cfg.package}/bin/overwitch-service";
+      #     Restart = "on-failure";
+      #   };
+      # };
+    };
   };
 }
