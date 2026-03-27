f8a53e98d3d38757b82518ea35a1ff93ed15e9f6
9Prestidigitator <9prestidigitator@gmail.com>
2026-03-27 19:07:51 -0400

    trying sddm theme

--- modules/sddm.nix
+++ modules/sddm.nix
@@ -1,9 +1,34 @@
 {
-  flake.nixosModules.sddm = {
+  flake.nixosModules.sddm = {pkgs, ...}: {
     services.displayManager.sddm = {
       enable = true;
       wayland.enable = true;
       settings.General.DisplayServer = "wayland";
+      theme = "sddm-astronaut-theme";
     };
+
+    environment.systemPackages = with pkgs; [
+      sddm-astronaut
+      kdePackages.qtmultimedia
+    ];
+    fonts.packages = with pkgs; [
+      inter
+    ];
+
+    environment.etc."sddm.conf.d/10-theme.conf".text = ''
+      [Theme]
+      Current=sddm-astronaut-theme
+
+      [General]
+      InputMethod=
+
+      [Users]
+      RememberLastUser=true
+      RememberLastSession=true
+    '';
+    #[Wayland]
+    #SessionDir=/run/current-system/sw/share/wayland-sessions
+    #[X11]
+    #SessionDir=/run/current-system/sw/share/xsessions
   };
 }
