4930b5a9cb39da73730390440579239a9ec9eb6b
9Prestidigitator <9Prestidigitator@gmail.com>
2026-04-12 13:18:09 -0400

    Much more nixcord option

--- modules/desktop/applications/communications.nix
+++ modules/desktop/applications/communications.nix
@@ -16,20 +16,33 @@
       config = {
         transparent = true;
         plugins = {
+          accountPanelServerProfile.enable = true;
           alwaysAnimate.enable = true;
           betterFolders.enable = true;
           BlurNSFW.enable = true;
+          callTimer.enable = true;
+          copyFileContents.enable = true;
+          CopyUserURLs.enable = true;
+          experiments.enable = true;
           fakeNitro = {
             enable = true;
             enableEmojiBypass = true;
             enableStickerBypass = true;
           };
           fixYoutubeEmbeds.enable = true;
+          friendsSince.enable = true;
           gifPaste.enable = true;
+          noMosaic.enable = true;
+          notificationVolume.enable = true;
           PinDMs.enable = true;
           volumeBooster.enable = true;
-          youtubeAdblock.enable = true;
+          relationshipNotifier.enable = true;
+          serverInfo.enable = true;
           spotifyCrack.enable = true;
+          showHiddenChannels.enable = true;
+          showHiddenThings.enable = true;
+          youtubeAdblock.enable = true;
+          whoReacted.enable = true;
         };
       };
     };
