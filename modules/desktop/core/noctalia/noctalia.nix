cc5a11ce6c9eef65a938762265e7403d95363385
9Prestidigitator <9Prestidigitator@gmail.com>
2026-04-15 01:02:18 -0400

    Updated screen recorder

--- modules/desktop/core/noctalia/noctalia.nix
+++ modules/desktop/core/noctalia/noctalia.nix
@@ -1,11 +1,11 @@
 {inputs, ...}: {
   flake.nixosModules.noctalia = {pkgs, ...}: {
     services.gnome.evolution-data-server.enable = true;
+    programs.gpu-screen-recorder.enable = true;
     environment.systemPackages = with pkgs; [
       (inputs.noctalia.packages.${pkgs.stdenv.hostPlatform.system}.default.override {
         calendarSupport = true;
       })
-      gpu-screen-recorder
       wlsunset
       wl-mirror
       cliphist
