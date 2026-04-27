020dc59da1f06dea424a2e1348147a1c54eaf132
9Prestidigitator <9Prestidigitator@gmail.com>
2026-04-27 12:59:32 -0400

    trying some stuff

--- modules/nixos/hardware/nvidia.nix
+++ modules/nixos/hardware/nvidia.nix
@@ -27,5 +27,12 @@
       enable = true;
       videoDrivers = ["nvidia"];
     };
+
+    environment.sessionVariables = {
+      GBM_BACKEND = "nvidia-drm";
+      __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+      LIBVA_DRIVER_NAME = "nvidia";
+      NVD_BACKEND = "direct";
+    };
   };
 }
