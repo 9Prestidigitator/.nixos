aa6dca256db62605e5813a99e2c8b1f29941f16d
9Prestidigitator <9prestidigitator@gmail.com>
2026-04-27 02:29:28 -0400

    testing wrapped program

--- modules/nixos/programs/terminal-tools.nix
+++ modules/nixos/programs/terminal-tools.nix
@@ -1,4 +1,4 @@
-{
+{self, ...}: {
   flake.nixosModules.terminal-tools = {pkgs, ...}: {
     programs = {
       git.enable = true;
@@ -6,60 +6,61 @@
         enable = true;
         presets = ["nerd-font-symbols"];
       };
-      tmux = {
-        enable = true;
-        shortcut = "a";
-        terminal = "tmux-256color";
-        newSession = true;
-        keyMode = "vi";
-        customPaneNavigationAndResize = true;
-        plugins = with pkgs; [
-          tmuxPlugins.resurrect
-          tmuxPlugins.continuum
-          tmuxPlugins.catppuccin
-          tmuxPlugins.yank
-        ];
-        escapeTime = 0;
-        extraConfigBeforePlugins = ''
-          set -g mouse on
-          set -g focus-events on
-          set -g history-limit 20000
-          set -g display-time 4000
-
-          bind-key -n C-p previous-window
-          bind-key -n C-n next-window
-          bind-key a last-window
-          bind-key P swap-window -t -1 -d
-          bind-key N swap-window -t +1 -d
-          bind-key Tab switch-client -l
-
-          set-option -g set-clipboard on
-          set-option -g status-position top
-          setw -g aggressive-resize on
-
-          set -g @continuum-restore 'on'
-
-          set -g @catppuccin_flavor "mocha"
-          set -g @catppuccin_window_status_style "basic"
-          set -g @catppuccin_status_background "none"
-          set -g @catppuccin_window_default_text " #W"
-          set -g @catppuccin_window_current_text " #W"
-          set -g @catppuccin_window_text " #W"
-        '';
-        extraConfig = ''
-          set -g status-left-length 100
-          set -g status-left ""
-
-          set -g status-right-length 100
-          set -g status-right "#{E:@catppuccin_status_session}"
-          set -ag status-right "#{E:@catppuccin_status_uptime}"
-          set -ag status-right "#{E:@catppuccin_status_date_time}"
-          set -agF status-right "#{E:@catppuccin_status_host}"
-        '';
-      };
+      # tmux = {
+      #   enable = true;
+      #   shortcut = "a";
+      #   terminal = "tmux-256color";
+      #   newSession = true;
+      #   keyMode = "vi";
+      #   customPaneNavigationAndResize = true;
+      #   plugins = with pkgs; [
+      #     tmuxPlugins.resurrect
+      #     tmuxPlugins.continuum
+      #     tmuxPlugins.catppuccin
+      #     tmuxPlugins.yank
+      #   ];
+      #   escapeTime = 0;
+      #   extraConfigBeforePlugins = ''
+      #     set -g mouse on
+      #     set -g focus-events on
+      #     set -g history-limit 20000
+      #     set -g display-time 4000
+      #
+      #     bind-key -n C-p previous-window
+      #     bind-key -n C-n next-window
+      #     bind-key a last-window
+      #     bind-key P swap-window -t -1 -d
+      #     bind-key N swap-window -t +1 -d
+      #     bind-key Tab switch-client -l
+      #
+      #     set-option -g set-clipboard on
+      #     set-option -g status-position top
+      #     setw -g aggressive-resize on
+      #
+      #     set -g @continuum-restore 'on'
+      #
+      #     set -g @catppuccin_flavor "mocha"
+      #     set -g @catppuccin_window_status_style "basic"
+      #     set -g @catppuccin_status_background "none"
+      #     set -g @catppuccin_window_default_text " #W"
+      #     set -g @catppuccin_window_current_text " #W"
+      #     set -g @catppuccin_window_text " #W"
+      #   '';
+      #   extraConfig = ''
+      #     set -g status-left-length 100
+      #     set -g status-left ""
+      #
+      #     set -g status-right-length 100
+      #     set -g status-right "#{E:@catppuccin_status_session}"
+      #     set -ag status-right "#{E:@catppuccin_status_uptime}"
+      #     set -ag status-right "#{E:@catppuccin_status_date_time}"
+      #     set -agF status-right "#{E:@catppuccin_status_host}"
+      #   '';
+      # };
     };
     environment.systemPackages = with pkgs; [
       bashInteractive
+      self.packages.${pkgs.system}.tmux
       btop
       vim
       lazygit
--- modules/nixos/system/sops.nix
+++ modules/nixos/system/sops.nix
@@ -2,7 +2,7 @@
   flake.nixosModules.sops = {pkgs, ...}: {
     imports = [inputs.sops-nix.nixosModules.sops];
     sops = {
-      defaultSopsFile = ../secrets/secrets.yaml;
+      defaultSopsFile = ../../../secrets.yaml;
       defaultSopsFormat = "yaml";
     };
     environment.systemPackages = with pkgs; [sops];
