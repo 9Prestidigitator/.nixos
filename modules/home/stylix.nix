af4d49ec6a9bfcf84d8e333e4f55266f0903fe98
9Prestidigitator <9prestidigitator@gmail.com>
2026-04-30 00:05:04 -0400

    trying to fix noctalia icons

--- modules/home/stylix.nix
+++ modules/home/stylix.nix
@@ -1,9 +1,15 @@
 {
-  flake.homeModules.stylix = {
+  flake.homeModules.stylix = {pkgs, ...}: {
     stylix = {
       targets = {
         spicetify.enable = false;
         neovim.enable = false;
+      };
+      icons = {
+        enable = true;
+        package = pkgs.kdePackages.breeze;
+        dark = "breeze-dark";
+        light = "breeze";
       };
     };
   };
