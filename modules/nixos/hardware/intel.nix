e6ba85a1ea593cff6d1cf65e954ad3eba49ef4f7
9Prestidigitator <9Prestidigitator@gmail.com>
2026-05-11 00:47:36 -0400

    updated flake

--- modules/nixos/hardware/intel.nix
+++ modules/nixos/hardware/intel.nix
@@ -3,8 +3,12 @@
     hardware = {
       graphics = {
         enable = true;
+        enable32Bit = true;
         extraPackages = with pkgs; [
           intel-media-driver
+          intel-vaapi-driver
+        ];
+        extraPackages32 = with pkgs.pkgsi686Linux; [
           intel-vaapi-driver
         ];
       };
