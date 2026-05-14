1b6e8f4743d0c6798b55308452df93d5841eb5f6
9Prestidigitator <9Prestidigitator@gmail.com>
2026-05-14 17:59:37 -0400

    updated lc command

--- modules/home/desktop/core/niri/_config/binds/launch.nix
+++ modules/home/desktop/core/niri/_config/binds/launch.nix
@@ -113,7 +113,7 @@ in {
           {
             key = "l";
             desc = "leetcode";
-            cmd = "kitty --title btop bash -lc 'nix develop ~/.nixos#default -c sh -c nvim leetcode.nvim'";
+            cmd = "kitty --title btop bash -lc 'nix develop ~/.nixos#default -c nvim leetcode.nvim'";
           }
           {
             key = "t";
