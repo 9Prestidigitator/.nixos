70ae44e04f9822097af9d823fa1e703f187de212
9Prestidigitator <9prestidigitator@gmail.com>
2026-05-15 17:20:14 -0400

    trying other options

--- modules/nixos/services/builder.nix
+++ modules/nixos/services/builder.nix
@@ -1,9 +1,11 @@
 {
   flake.nixosModules.builder = {
     users.users.nixremote = {
-      home = "/home/nixremote";
+      isNormalUser = true;
+
       group = "users";
       extraGroups = [ ];
+
       hashedPassword = "!";
       openssh.authorizedKeys.keys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMh97+2Z+enIYqz/OPrzjvLfD5wcrYSX+QXDh45qjZDN"];
     };
