3a7a3d7847d32ba244447c69774b500482db709e
9Prestidigitator <9prestidigitator@gmail.com>
2026-05-15 17:17:36 -0400

    updating builder

--- modules/nixos/services/builder.nix
+++ modules/nixos/services/builder.nix
@@ -2,6 +2,8 @@
   flake.nixosModules.builder = {
     users.users.nixremote = {
       home = "/home/nixremote";
+      group = "users";
+      extraGroups = [ ];
       hashedPassword = "!";
       openssh.authorizedKeys.keys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMh97+2Z+enIYqz/OPrzjvLfD5wcrYSX+QXDh45qjZDN"];
     };
