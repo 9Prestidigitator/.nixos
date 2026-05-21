8328fbff42e6b31c5606ca52f2f7f305f8dc11d3
9Prestidigitator <9prestidigitator@gmail.com>
2026-05-21 02:23:08 -0400

    trying more plasma stuff

--- modules/home/desktop/core/plasma.nix
+++ modules/home/desktop/core/plasma.nix
@@ -16,6 +16,40 @@
           TerminalService = "kitty.desktop";
         };
       };
+      panels = [
+        {
+          location = "bottom";
+          widgets = [
+            {
+              iconTasks = {
+                launchers = [
+                  "applications:kitty.desktop"
+                  "applications:org.kde.dolphin.desktop"
+                  "applications:brave-browser.desktop"
+                ];
+              };
+            }
+
+            "org.kde.plasma.panelspacer"
+
+            {
+              kickoff = {
+                icon = "nix-snowflake-white";
+                sortAlphabetically = true;
+              };
+            }
+
+            "org.kde.plasma.panelspacer"
+
+            "org.kde.plasma.systemtray"
+            {
+              digitalClock = {
+                time.format = "24h";
+              };
+            }
+          ];
+        }
+      ];
     };
   };
 }
