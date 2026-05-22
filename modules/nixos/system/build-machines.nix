262315e99186140c5bc46d1a7fdf33039be50f54
9Prestidigitator <9prestidigitator@gmail.com>
2026-05-21 21:59:35 -0400

    making builder a known host on local machines

--- modules/nixos/system/build-machines.nix
+++ modules/nixos/system/build-machines.nix
@@ -2,6 +2,12 @@
   flake.nixosModules.ink-build-machine = {config, ...}: {
     imports = [self.nixosModules.sops];
 
+    programs.ssh.knownHosts.ink = {
+      hostNames = ["10.123.78.170" "ink"];
+      # You can find this at /etc/ssh/ssh_host_<type>_key.pub
+      publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ0Y/FJPDKzaGNR/XeqeSZ4IzaJp+PiCgifWPwishRUo root@ink";
+    };
+
     sops.secrets."ssh/builders/ink" = {
       sopsFile = ../../../secrets/builders.yaml;
       owner = "root";
@@ -17,12 +23,11 @@
           hostName = "10.123.78.170";
           sshUser = "nixremote";
           sshKey = config.sops.secrets."ssh/builders/ink".path;
-
-          systems = [
-            "x86_64-linux"
-            "builtin"
-          ];
           protocol = "ssh-ng";
+          # You can find via base64 -w0 /etc/ssh/ssh_host_<type>_key.pub
+          publicHostKey = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSUowWS9GSlBES3phR05SL1hlcWVTWjRJemFKcCtQaUNnaWZXUHdpc2hSVW8gcm9vdEBpbmsK";
+
+          systems = ["x86_64-linux" "builtin"];
           maxJobs = 8;
           speedFactor = 3;
           supportedFeatures = [
