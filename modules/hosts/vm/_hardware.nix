83d23f1515524445b3b241ce91df6c8aa4c5c303
9Prestidigitator <9Prestidigitator@gmail.com>
2026-05-24 01:20:24 -0400

    back to normal

--- modules/hosts/vm/_hardware.nix
+++ modules/hosts/vm/_hardware.nix
@@ -7,27 +7,8 @@
 
   boot.initrd.availableKernelModules = ["ahci" "xhci_pci" "virtio_pci" "virtio_blk" "usb_storage" "sd_mod" "sr_mod"];
   boot.initrd.kernelModules = [];
-  boot.kernelModules = ["kvm-intel"];
+  boot.kernelModules = ["kvm-intel" "kvm-amd"];
   boot.extraModulePackages = [];
-
-  services.qemuGuest.enable = true;
-  services.spice-vdagentd.enable = true;
-
-  hardware.graphics = {
-    enable = true;
-    enable32Bit = true;
-  };
-
-  services.xserver = {
-    enable = true;
-    videoDrivers = ["modesetting"];
-  };
-
-  environment.sessionVariables = {
-    # Makes GNOME/Qt usable when the VM GPU has broken accel
-    LIBGL_ALWAYS_SOFTWARE = "1";
-    GSK_RENDERER = "cairo";
-  };
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 }
