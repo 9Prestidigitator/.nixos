b78bd7cb1e9366d5c98bea48592811b8eb2e5eb2
9Prestidigitator <9prestidigitator@gmail.com>
2026-05-24 14:37:26 -0400

    disko surface

---
+++ modules/hosts/surface/disko.nix
@@ -0,0 +1,94 @@
+{
+  flake.diskoConfigurations.surface = {
+    disko.devices = {
+      disk.main = {
+        device = "/dev/nvme0n1";
+        type = "disk";
+
+        content = {
+          type = "gpt";
+
+          partitions = {
+            boot = {
+              name = "boot";
+              size = "1M";
+              type = "EF02";
+            };
+
+            esp = {
+              name = "ESP";
+              size = "1G";
+              type = "EF00";
+
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [
+                  "umask=0077"
+                ];
+              };
+            };
+
+            swap = {
+              name = "swap";
+              size = "8G";
+              content = {
+                type = "swap";
+                randomEncryption = true;
+              };
+            };
+
+            root = {
+              name = "root";
+              size = "100%";
+              content = {
+                type = "luks";
+                name = "crypted-root";
+                settings.allowDiscards = true;
+                content = {
+                  type = "btrfs";
+                  extraArgs = [
+                    "-f"
+                    "-L"
+                    "nixos"
+                  ];
+                  subvolumes = {
+                    "@root" = {
+                      mountpoint = "/";
+                      mountOptions = [
+                        "compress=zstd"
+                        "noatime"
+                      ];
+                    };
+                    "@nix" = {
+                      mountpoint = "/nix";
+                      mountOptions = [
+                        "compress=zstd"
+                        "noatime"
+                      ];
+                    };
+                    "@persist" = {
+                      mountpoint = "/persist";
+                      mountOptions = [
+                        "compress=zstd"
+                        "noatime"
+                      ];
+                    };
+                    "@log" = {
+                      mountpoint = "/var/log";
+                      mountOptions = [
+                        "compress=zstd"
+                        "noatime"
+                      ];
+                    };
+                  };
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
