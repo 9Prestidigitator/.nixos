572271f4310faca39ad562f4fb06295080c903ac
9Prestidigitator <9prestidigitator@gmail.com>
2026-05-25 18:38:21 -0400

    better builder secrets structure

--- modules/nixos/nix/build-machines.nix
+++ modules/nixos/nix/build-machines.nix
@@ -2,13 +2,6 @@
   flake.nixosModules.ink-build-machine = {config, ...}: {
     imports = [self.nixosModules.sops];
 
-    sops.secrets."ssh/builders/ink" = {
-      sopsFile = ../../../secrets/builders.yaml;
-      owner = "root";
-      group = "root";
-      mode = "0400";
-    };
-
     programs.ssh.knownHosts.ink = {
       hostNames = ["10.123.78.170" "ink"];
       publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ0Y/FJPDKzaGNR/XeqeSZ4IzaJp+PiCgifWPwishRUo root@ink"; # You can find this at /etc/ssh/ssh_host_<type>_key.pub
@@ -21,7 +14,7 @@
         {
           hostName = "10.123.78.170";
           sshUser = "nixremote";
-          sshKey = config.sops.secrets."ssh/builders/ink".path;
+          sshKey = config.sops.secrets."ssh/builders".path;
           protocol = "ssh-ng";
           # You can find via base64 -w0 /etc/ssh/ssh_host_<type>_key.pub
           publicHostKey = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSUowWS9GSlBES3phR05SL1hlcWVTWjRJemFKcCtQaUNnaWZXUHdpc2hSVW8gcm9vdEBpbmsK";
@@ -43,13 +36,6 @@
   flake.nixosModules.surface-build-machine = {config, ...}: {
     imports = [self.nixosModules.sops];
 
-    sops.secrets."ssh/builders/ink" = {
-      sopsFile = ../../../secrets/builders.yaml;
-      owner = "root";
-      group = "root";
-      mode = "0400";
-    };
-
     programs.ssh.knownHosts.surface = {
       hostNames = ["10.123.78.31" "surface"];
       publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGsqWpspLPQaZ0ck74fbjW1F5E+GG7Qk0gZuJ0crUCWR root@surface"; # You can find this at /etc/ssh/ssh_host_<type>_key.pub
@@ -62,7 +48,7 @@
         {
           hostName = "10.123.78.31";
           sshUser = "nixremote";
-          sshKey = config.sops.secrets."ssh/builders/ink".path;
+          sshKey = config.sops.secrets."ssh/builders".path;
           protocol = "ssh-ng";
           # You can find via base64 -w0 /etc/ssh/ssh_host_<type>_key.pub
           publicHostKey = "c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSUdzcVdwc3BMUFFhWjBjazc0ZmJqVzFGNUUrR0c3UWswZ1p1SjBjclVDV1Igcm9vdEBzdXJmYWNlCg==";
@@ -73,7 +59,6 @@
           supportedFeatures = [
             "nixos-test"
             "benchmark"
-            "big-parallel"
             "kvm"
           ];
         }
--- modules/nixos/nix/sops.nix
+++ modules/nixos/nix/sops.nix
@@ -4,7 +4,15 @@
     sops = {
       age.keyFile = "/etc/sops/age/keys.txt";
       defaultSopsFormat = "yaml";
+
+      secrets."ssh/builders" = {
+        sopsFile = ../../../secrets/builders.yaml;
+        owner = "root";
+        group = "root";
+        mode = "0400";
+      };
     };
+
     environment.systemPackages = with pkgs; [sops age];
 
     persist.directories = ["/etc/sops"];
