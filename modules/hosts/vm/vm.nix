012a65061db137cab5a2cc2a029e7b78054152ff
9Prestidigitator <9prestidigitator@gmail.com>
2026-06-03 19:35:12 -0400

    back to preservation

--- modules/hosts/vm/vm.nix
+++ modules/hosts/vm/vm.nix
@@ -15,7 +15,7 @@
 
       nixosModules.ink-build-machine
       nixosModules.stylix
-      nixosModules.impermanence
+      nixosModules.preservation
       nixosModules.home-manager
       nixosModules.nix
 
