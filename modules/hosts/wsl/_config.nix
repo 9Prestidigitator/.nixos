570d058a377562f44551eb1729e3cf06e70bb849
9Prestidigitator <9Prestidigitator@gmail.com>
2026-06-06 15:10:42 -0400

    Trying to disable some stuff

--- modules/hosts/wsl/_config.nix
+++ modules/hosts/wsl/_config.nix
@@ -1,4 +1,8 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  lib,
+  ...
+}: {
   host.name = "wsl";
   home-manager.users.max = {self, ...}: {
     imports = with self; [
@@ -17,6 +21,18 @@
     enable = true;
     defaultUser = "max";
     startMenuLaunchers = true;
+  };
+
+  networking = {
+    networkmanager.enable = lib.mkForce false;
+    wireless.enable = lib.mkForce false;
+    useDHCP = lib.mkForce false;
+  };
+
+  systemd.services = {
+    wpa_supplicant.enable = lib.mkForce false;
+    NetworkManager.enable = lib.mkForce false;
+    NetworkManager-dispatcher.enable = lib.mkForce false;
   };
 
   environment.systemPackages = with pkgs; [usbutils kmod cryptsetup];
