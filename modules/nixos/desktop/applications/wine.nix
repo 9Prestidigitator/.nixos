7235ca09fe6d8de670c304490913051b2c687f09
9Prestidigitator <9Prestidigitator@gmail.com>
2026-06-23 11:29:18 -0400

    Trying out this approach to wine globally

--- modules/nixos/desktop/applications/wine.nix
+++ modules/nixos/desktop/applications/wine.nix
@@ -1,13 +1,39 @@
 {
   flake.nixosModules.wine = {pkgs, ...}: {
-    environment.systemPackages = with pkgs; [
-      wineWow64Packages.yabridge
-      yabridge
-      yabridgectl
-      winetricks
-      winePackages.fonts
-      wineasio
-    ];
+    environment.systemPackages = let
+      latestWineForYabridge = pkgs.wineWow64Packages.base.override {wineRelease = "staging";};
+
+      # latestWineForYabridge =
+      #   (
+      #     pkgs.wineWow64Packages.base.override {wineRelease = "staging";}
+      #   ).overrideAttrs (old: {
+      #     patches =
+      #       (old.patches or [])
+      #       ++ [
+      #         (pkgs.fetchurl {
+      #           url = "https://gitlab.winehq.org/-/project/5/uploads/dea8a1e711846f7e7642c16eacd284b4/bug51357.patch";
+      #           hash = "sha256-ZfW94gCDLGauKEZOid7ndQsaPA6SVGk22CQ3EBWAPm8=";
+      #         })
+      #       ];
+      #   });
+
+      wineSetForYabridge = pkgs.wineWow64Packages // {yabridge = latestWineForYabridge;};
+
+      yabridgePatched = pkgs.yabridge.override {wineWow64Packages = wineSetForYabridge;};
+      yabridgectlPatched = pkgs.yabridgectl.override {
+        wineWow64Packages = wineSetForYabridge;
+        yabridge = yabridgePatched;
+      };
+    in
+      with pkgs; [
+        latestWineForYabridge
+        yabridgePatched
+        yabridgectlPatched
+        winetricks
+        winePackages.fonts
+        wineasio
+      ];
+
     persist.userDirs = [
       ".wine"
       ".local/share/applications/wine"
