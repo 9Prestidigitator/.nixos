d4501c8f058b6ce19b6bf4c33005b18ca23b60f5
9Prestidigitator <9Prestidigitator@gmail.com>
2026-06-25 18:28:17 -0400

    more leetcode convenience

--- modules/home/programs/terminal-tools.nix
+++ modules/home/programs/terminal-tools.nix
@@ -118,7 +118,7 @@
         fman = "compgen -c | sort -hr | fzf | xargs man";
         # This is useful for impermanent systems
         lsblk = "lsblk -e7 -o NAME,SIZE,TYPE,FSTYPE,LABEL,UUID,MOUNTPOINT";
-        leet = "nvim leetcode.nvim";
+        leet = "nix develop ${config.home.homeDirectory}/.nixos#python -c sh -c 'nvim leetcode.nvim'";
 
         nhre = "nh os switch ${config.home.homeDirectory}/.nixos -H ${osConfig.networking.hostName}";
         nhbo = "nh os boot ${config.home.homeDirectory}/.nixos -H ${osConfig.networking.hostName}";
