9f60364dbca0c396cc2c2d76139dd7da421ad609
9Prestidigitator <9Prestidigitator@gmail.com>
2026-08-01 14:39:19 -0400

    More reaper extensions

--- modules/home/desktop/applications/reaper/_config/extensions.nix
+++ modules/home/desktop/applications/reaper/_config/extensions.nix
@@ -7,7 +7,19 @@
         repositories = [
           {
             name = "reaper-keys";
-            url = "https://raw.githubusercontent.com/gwatcha/reaper-keys/master/index.xml";
+            url = "https://github.com/gwatcha/reaper-keys/raw/master/index.xml";
+          }
+          {
+            name = "Sexan_Scripts";
+            url = "https://github.com/GoranKovac/ReaScripts/raw/master/index.xml";
+          }
+          {
+            name = "Helgobox";
+            url = "https://github.com/helgoboss/reaper-packages/raw/master/index.xml";
+          }
+          {
+            name = "Bird-Bird";
+            url = "https://github.com/Bird-Bird/ReaScript_Testing/raw/main/index.xml";
           }
         ];
 
@@ -16,6 +28,26 @@
             repository = "MPL Scripts";
             category = "FX";
             name = "mpl_Toggle bypass FX with latency (PDC) higher than X samples.lua";
+          }
+          {
+            repository = "ReaTeam Extensions";
+            category = "API";
+            name = "js_ReaScriptAPI.ext";
+          }
+          {
+            repository = "ReaTeam Extensions";
+            category = "API";
+            name = "reaper_imgui.ext";
+          }
+          {
+            repository = "Sexan_Scripts";
+            category = "ParanormalFX";
+            name = "Sexan_ParaNormal_FX_Router.lua";
+          }
+          {
+            repository = "Bird-Bird";
+            category = "Global Sampler";
+            name = "BirdBird_Global Sampler.lua";
           }
         ];
 
