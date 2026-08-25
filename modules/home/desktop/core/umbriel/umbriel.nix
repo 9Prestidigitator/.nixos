c6dee6014234200377662b4502680fb437b5ae0f
9Prestidigitator <9prestidigitator@gmail.com>
2026-08-25 00:22:15 -0400

    Trying out more binds

--- modules/home/desktop/core/umbriel/umbriel.nix
+++ modules/home/desktop/core/umbriel/umbriel.nix
@@ -20,8 +20,30 @@
           natural_scroll = true;
         };
         keybinds = {
-          "Mod+Return" = "spawn:kitty";
+          "Mod+Left" = "window-focus-left";
+          "Mod+Right" = "window-focus-right";
+          "Mod+Up" = "window-focus-up";
+          "Mod+Down" = "window-focus-down";
+
+          "Mod+H" = "window-focus-left";
+          "Mod+L" = "window-focus-right";
+          "Mod+K" = "window-focus-up";
+          "Mod+J" = "window-focus-down";
+
           "Mod+Q" = "window-close";
+          "Mod+O" = "overview-toggle";
+          "Mod+Ctrl+Space" = "window-toggle-floating";
+          "Mod+F" = "window-toggle-maximize-to-edges";
+          "Mod+Shift+F" = "window-toggle-fullscreen";
+          "Mod+R" = "window-cycle-width";
+
+          "Mod+Minus" = "window-modify-width:-0.1";
+          "Mod+Equal" = "window-modify-width:0.1";
+
+          "Mod+PageUp" = "workspace-previous";
+          "Mod+U" = "workspace-previous";
+          "Mod+PageDown" = "workspace-next";
+          "Mod+D" = "workspace-next";
         };
         appearance.blur = {
           enabled = true;
