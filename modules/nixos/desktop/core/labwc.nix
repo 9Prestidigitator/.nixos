a528596a99713d9664f53c4c31b8af363352a5f1
9Prestidigitator <9prestidigitator@gmail.com>
2026-08-28 01:38:45 -0400

    added env vars to labwc

--- modules/nixos/desktop/core/labwc.nix
+++ modules/nixos/desktop/core/labwc.nix
@@ -11,6 +11,12 @@
 
         xdg.portal.enable = true;
 
+        environment.sessionVariables = {
+          NIXOS_OZONE_WL = "1";
+          MOZ_ENABLE_WAYLAND = "1";
+          SDL_VIDEODRIVER = "wayland";
+        };
+
         environment.systemPackages = with pkgs; [
           wdisplays
           wl-clipboard
