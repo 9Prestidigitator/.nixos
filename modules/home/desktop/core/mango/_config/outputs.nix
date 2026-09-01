d0e59f02cb9b4dcbf7a420a0a0a4444a1093329e
9Prestidigitator <9prestidigitator@gmail.com>
2026-09-01 00:15:40 -0400

    updated outputs

--- modules/home/desktop/core/mango/_config/outputs.nix
+++ modules/home/desktop/core/mango/_config/outputs.nix
@@ -5,8 +5,15 @@
 }: let
   hostName = osConfig.networking.hostName;
 in {
-  wayland.windowManager.mango.settings.monitorrule = lib.optionals (hostName == "papyr") [
-    "name:^eDP-1$,width:1920,height:1080,x:0,y:0,scale:1.3"
-    "name:^HDMI-A-1$,scale:1.4"
+  wayland.windowManager.mango.settings.monitorrule = lib.concatLists [
+    (lib.optionals (hostName == "ink") [
+      "make:Microstep,model:MSI G27C4,serial:0x30333758,width:1920,height:1080,refresh:165.003,x:1600,y:0,scale:1"
+      "make:Hewlett Packard,model:HP 22cwa,serial:6CM82100S4,width:1920,height:1080,refresh:60,x:0,y:0,scale:1.2"
+    ])
+
+    (lib.optionals (hostName == "papyr") [
+      "name:^eDP-1$,width:1920,height:1080,x:0,y:0,scale:1.3"
+      "name:^HDMI-A-1$,scale:1.4"
+    ])
   ];
 }
